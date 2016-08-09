45435c714b70bda3cd306ff5eb077f7f2c5e0e8a
ClimbsRocks <ClimbsBytes@gmail.com>
2016-08-08 18:41:21 -0700

    modularizes to take in 'full' or 'short' as cli arguments, and load the appropriate dataset

--- test_script.py
+++ test_script.py
@@ -1,41 +1,46 @@
 import csv
 import random
+import sys
+
 from auto_ml import Predictor
 from auto_ml import utils
 
-# open full dataset
-with open('numerai_datasets_early_aug/numerai_training_data.csv', 'rU') as input_file:
-    training_rows = csv.DictReader(input_file)
+if sys.argv[2] in ('full', 'long', 'full_dataset', 'all_data', 'all'):
 
-    training_data = []
+    # open full dataset
+    with open('numerai_datasets_early_aug/numerai_training_data.csv', 'rU') as input_file:
+        training_rows = csv.DictReader(input_file)
 
-    testing_data = []
+        training_data = []
 
-    for row in training_rows:
-        if random.random() > 0.8:
-            testing_data.append(row)
-        else:
-            training_data.append(row)
+        testing_data = []
 
-# # write short dataset to file
-# with open('numerai_datasets_early_aug/numerai_short.csv', 'w+') as write_file:
-#     writer = csv.writer(write_file)
-#     writer.writerow(["feature1","feature2","feature3","feature4","feature5","feature6","feature7","feature8","feature9","feature10","feature11","feature12","feature13","feature14","feature15","feature16","feature17","feature18","feature19","feature20","feature21","target"])
-#     for row in training_data_short:
-#         writer.writerow(row)
+        for row in training_rows:
+            if random.random() > 0.8:
+                testing_data.append(row)
+            else:
+                training_data.append(row)
 
-# # load short dataset
-# with open('numerai_datasets_early_aug/numerai_short.csv', 'rU') as input_file:
-#     training_rows = csv.DictReader(input_file)
+    # # write short dataset to file
+    # with open('numerai_datasets_early_aug/numerai_short.csv', 'w+') as write_file:
+    #     writer = csv.writer(write_file)
+    #     writer.writerow(["feature1","feature2","feature3","feature4","feature5","feature6","feature7","feature8","feature9","feature10","feature11","feature12","feature13","feature14","feature15","feature16","feature17","feature18","feature19","feature20","feature21","target"])
+    #     for row in training_data_short:
+    #         writer.writerow(row)
 
-#     training_data = []
-#     testing_data = []
+else:
+    # load short dataset
+    with open('numerai_datasets_early_aug/numerai_short.csv', 'rU') as input_file:
+        training_rows = csv.DictReader(input_file)
 
-#     for row in training_rows:
-#         if random.random() > 0.8:
-#             testing_data.append(row)
-#         else:
-#             training_data.append(row)
+        training_data = []
+        testing_data = []
+
+        for row in training_rows:
+            if random.random() > 0.8:
+                testing_data.append(row)
+            else:
+                training_data.append(row)
 
 
 def test_input_func(X, y=None):
@@ -45,14 +50,14 @@ def test_input_func(X, y=None):
     print('inside test_input_func')
     return X
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
-    ml_predictor = Predictor(type_of_algo='classifier', column_descriptions={'target': 'output'})
+ml_predictor = Predictor(type_of_algo='classifier', column_descriptions={'target': 'output'})
 
-    ml_predictor.train(training_data, user_input_func=test_input_func, grid_search=True)
+ml_predictor.train(training_data, user_input_func=test_input_func, grid_search=True)
 
-    output_splitter = utils.SplitOutput('target')
-    X_test, y_test = output_splitter.transform(testing_data)
+output_splitter = utils.SplitOutput('target')
+X_test, y_test = output_splitter.transform(testing_data)
 
-    ml_predictor.predict_proba(X_test)
-    print(ml_predictor.score(X_test, y_test))
+ml_predictor.predict_proba(X_test)
+print(ml_predictor.score(X_test, y_test))
