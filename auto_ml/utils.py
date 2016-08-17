3b2d288c20dfead9e814faca13982ecaecb46bce
ClimbsRocks <ClimbsBytes@gmail.com>
2016-08-16 18:27:31 -0700

    creates feature selection transformer class

--- auto_ml/utils.py
+++ auto_ml/utils.py
@@ -4,8 +4,9 @@ import os
 
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.feature_selection import GenericUnivariateSelect, RFECV, SelectFromModel
 from sklearn.grid_search import GridSearchCV, RandomizedSearchCV
-from sklearn.linear_model import LogisticRegression, LinearRegression, RidgeClassifier, Ridge
+from sklearn.linear_model import LogisticRegression, LinearRegression, RandomizedLasso, RandomizedLogisticRegression, RidgeClassifier, Ridge
 from sklearn.metrics import mean_squared_error, make_scorer, brier_score_loss
 
 import scipy
@@ -277,3 +278,51 @@ def write_most_recent_gs_result_to_file(trained_gs, most_recent_filename, timest
             writer.writerow(header_row)
         for row in rows_to_write:
             writer.writerow(row)
+
+
+class FeatureSelectionTransformer(BaseEstimator, TransformerMixin):
+
+
+
+    def __init__(self, type_of_model, feature_selection_model='SelectFromModel'):
+
+
+        self.type_of_model = type_of_model
+        # TODO(PRESTON): optimize the params used here
+        self._model_map = {
+            'classifier': {
+                'SelectFromModel': SelectFromModel(RandomForestClassifier(n_jobs=-1)),
+                'RFECV': RFECV(estimator=RandomForestClassifier(n_jobs=-1), step=0.1),
+                'GenericUnivariateSelect': GenericUnivariateSelect(),
+                'RandomizedSearch': RandomizedLogisticRegression()
+            },
+            'regressor': {
+                'SelectFromModel': SelectFromModel(RandomForestRegressor(n_jobs=-1)),
+                'RFECV': RFECV(estimator=RandomForestRegressor(n_jobs=-1), step=0.1),
+                'GenericUnivariateSelect': GenericUnivariateSelect(),
+                'RandomizedSearch': RandomizedLasso()
+            }
+        }
+
+        print('self.type_of_model')
+        print(self.type_of_model)
+        print('feature_selection_model')
+        print(feature_selection_model)
+        # ['SelectFromModel', 'RFECV', 'GenericUnivariateSelect', 'RandomizedSearch']
+
+        # TODO(PRESTON): build in different params for regressors and classifiers
+        # TODO(PRSTON): eventually let threshold be user-configurable (or grid_searchable)
+        # TODO(PRESTON): verify we can use max in threshold below. if not, just hardcode in some values depending on the estimator used.
+        self.selector = self._model_map[self.type_of_model][feature_selection_model]
+
+
+
+    def fit(self, X, y=None):
+        # TODO(PRESTON): get feature names that were selected.
+        #   Then make sure to get feature_names from this transformer rather than DV, if this transformer exists
+        self.selector.fit(X, y)
+        return self
+
+
+    def transform(self, X, y=None):
+        return self.selector.transform(X)
