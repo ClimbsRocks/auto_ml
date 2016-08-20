922f4d253a1ce84314cefc4e32b7d5275b33df2a
ClimbsRocks <ClimbsBytes@gmail.com>
2016-08-20 15:13:37 -0700

    adds initial round of logic for RobustSparseScaler

--- auto_ml/utils.py
+++ auto_ml/utils.py
@@ -392,4 +392,76 @@ def rmse_scoring(estimator, X, y, took_log_of_y=False):
     return rmse
 
 
+def get_all_attribute_names(list_of_dictionaries, cols_to_avoid):
+    attribute_hash = {}
+    for dictionary in list_of_dictionaries:
+        for k, v in dictionary:
+            attribute_hash[k] = True
+
+    # All of the columns in column_descriptions should be avoided. They're probably either categorical or NLP data, both of which cannot be scaled.
+
+    attribute_list = [k for k, v in attribute_hash.items() if k not in cols_to_avoid]
+    return attribute_list
+
+
+# Scale sparse data to the 90th and 10th percentile
+# Only do so for values that actuall exist (do absolutely nothing with rows that do not have this data point)
+def CustomSparseScaler(BaseEstimator, TransformerMixin):
+
+
+    def __init__(self, column_descriptions):
+        self.column_descriptions = column_descriptions
+        self.cols_to_avoid = set([k for k, v in column_descriptions.items()])
+        pass
+
+
+    def fit(self, X, y=None):
+        attribute_list = get_all_attribute_names(X, self.column_descriptions)
+
+        attributes_per_round = [[], [], []]
+
+        attributes_summary = {}
+
+        # Randomly assign each attribute to one of three buckets
+        # We will summarize the data in three separate iterations, to avoid duplicating too much data in memory at any one point.
+        for attribute in attribute_list:
+            bucket_idx = int(random.random() * 3)
+            attributes_per_round[bucket_idx].append(attribute)
+            attributes_summary[attribute] = []
+
+        for bucket in attributes_per_round:
+
+            attributes_to_summarize = set(bucket)
+
+            for row in X:
+                for k, v in row.items():
+                    attributes_summary[k].append(v)
+
+            for attribute in bucket:
+
+                # Sort our collected data for that column
+                attributes_summary[attribute] = sorted(attributes_summary[attribute])
+                col_vals = attributes_summary[attribute]
+                tenth_percentile = col_vals[int(0.1 * len(col_vals))]
+                ninetieth_percentile = col_vals[int(0.9 * len(col_vals))]
+                attributes_summary[attribute] = [tenth_percentile, ninetieth_percentile, ninetieth_percentile - tenth_percentile]
+                del col_vals
+
+        self.attributes_summary = attributes_summary
+
+
+    # Perform basic min/max scaling, with the minor caveat that our min and max values are the 10th and 90th percentile values, to avoid outliers.
+    def transform(self, X, y=None):
+        for row in X:
+            for k, v in row.items():
+                if k not in self.cols_to_avoid:
+                    min_val = self.attributes_summary[k][0]
+                    max_val = self.attributes_summary[k][1]
+                    attribute_range = self.attributes_summary[k][2]
+                    row[k] = (v - min_val) / attribute_range
+        print(X[10])
+        print(X[100])
+
+        return X
+
 
