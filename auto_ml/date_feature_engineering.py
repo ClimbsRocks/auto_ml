eb2a892a391b55f11a8a2fae180cb8e6fa4afaf1
ClimbsRocks <climbsbytes@gmail.com>
2016-10-01 13:38:41 -0700

    more flexibility in the formats for dates, and a bugfix

--- auto_ml/date_feature_engineering.py
+++ auto_ml/date_feature_engineering.py
@@ -1,4 +1,5 @@
 import datetime
+from dateutil import parser
 
 from sklearn.base import BaseEstimator, TransformerMixin
 
@@ -17,33 +18,36 @@ class FeatureEngineer(BaseEstimator, TransformerMixin):
     def extract_features(self, row, date_val, date_col):
 
         row[date_col + '_day_of_week'] = str(date_val.weekday())
-        row[date_col + '_hour'] = date_val.hour
 
-        minutes_into_day = date_val.hour * 60 + date_val.minute
+        if isinstance(date_val, datetime.datetime):
+            row[date_col + '_hour'] = date_val.hour
+
+            minutes_into_day = date_val.hour * 60 + date_val.minute
 
         if row[date_col + '_day_of_week'] in (5,6):
             row[date_col + '_is_weekend'] = True
-        elif row[date_col + '_day_of_week'] == 4 and row[date_col + '_hour'] > 16:
+        elif row[date_col + '_day_of_week'] == 4 and row.get(date_col + '_hour', 0) > 16:
             row[date_col + '_is_weekend'] = True
         else:
             row[date_col + '_is_weekend'] = False
 
-            # Grab rush hour times for the weekdays.
-            # We are intentionally not grabbing them for the weekends, since weekend behavior is likely very different than weekday behavior.
-            if minutes_into_day < 120:
-                row[date_col + '_is_late_night'] = True
-            elif minutes_into_day < 11.5 * 60:
-                row[date_col + '_is_off_peak'] = True
-            elif minutes_into_day < 13.5 * 60:
-                row[date_col + '_is_lunch_rush_hour'] = True
-            elif minutes_into_day < 17.5 * 60:
-                row[date_col + '_is_off_peak'] = True
-            elif minutes_into_day < 20 * 60:
-                row[date_col + '_is_dinner_rush_hour'] = True
-            elif minutes_into_day < 22.5 * 60:
-                row[date_col + '_is_off_peak'] = True
-            else:
-                row[date_col + '_is_late_night'] = True
+            if isinstance(date_val, datetime.datetime):
+                # Grab rush hour times for the weekdays.
+                # We are intentionally not grabbing them for the weekends, since weekend behavior is likely very different than weekday behavior.
+                if minutes_into_day < 120:
+                    row[date_col + '_is_late_night'] = True
+                elif minutes_into_day < 11.5 * 60:
+                    row[date_col + '_is_off_peak'] = True
+                elif minutes_into_day < 13.5 * 60:
+                    row[date_col + '_is_lunch_rush_hour'] = True
+                elif minutes_into_day < 17.5 * 60:
+                    row[date_col + '_is_off_peak'] = True
+                elif minutes_into_day < 20 * 60:
+                    row[date_col + '_is_dinner_rush_hour'] = True
+                elif minutes_into_day < 22.5 * 60:
+                    row[date_col + '_is_off_peak'] = True
+                else:
+                    row[date_col + '_is_late_night'] = True
 
         return row
 
@@ -69,15 +73,19 @@ class FeatureEngineer(BaseEstimator, TransformerMixin):
 
                     # make sure that value is actually a datetime object
                     if type(date_val) not in (datetime.date, datetime.datetime):
-                        print('This value is not a date:')
-                        print(date_val)
                         try:
-                            date_val = datetime.datetime(date_val)
+                            date_val = str(date_val)
+                            date_val = parser.parse(date_val)
+                            # date_val = datetime.datetime(date_val)
+                            row_copy = self.extract_features(row_copy, date_val, date_col)
                         except:
+                            print('This value is not a date:')
+                            print(date_val)
+                            print('This value\'s type:')
+                            print(type(date_val))
                             pass
 
-                    row_copy = self.extract_features(row_copy, date_val, date_col)
-                    X_copy.append(row_copy)
+            X_copy.append(row_copy)
 
         return X_copy
 
