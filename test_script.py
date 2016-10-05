5a0cfe32a0c4c08d8188da1b9df95faec8cc81bc
Manikandan.R <vishnumani.2009@gmail.com>
2016-10-05 21:01:07 +0530

    Added tests to describe dataframe usage or dict object usage

--- test_script.py
+++ test_script.py
@@ -4,6 +4,7 @@ import sys
 import pandas as pd
 from auto_ml import Predictor
 from auto_ml import utils
+random.seed(10)
 
 if len(sys.argv) > 1 and sys.argv[1] in set(['full', 'long', 'full_dataset', 'all_data', 'all']):
 
@@ -30,7 +31,7 @@ if len(sys.argv) > 1 and sys.argv[1] in set(['full', 'long', 'full_dataset', 'al
 
 else:
     # load short dataset
-    with open('iris.csv', 'rU') as input_file:
+    with open('numerai_training_data.csv', 'rU') as input_file:
         training_rows = csv.DictReader(input_file)
 
         training_data = []
@@ -43,23 +44,31 @@ else:
                 training_data.append(row)
 
 # print type(pd)
-training_data=pd.DataFrame.from_dict(training_data)
 
-testing_data=pd.DataFrame.from_dict(testing_data)
+print "Testing with dict objects"
+
 ml_predictor = Predictor(type_of_estimator='classifier', column_descriptions={'sentence':'text','target': 'output'})
 
-#print (training_data.loc[:,['target']].values)
-#print training_data
-# split out out output column so we have a proper X, y dataset
-
 X_test, y_test = utils.split_output_dataframe(testing_data, output_column_name='target')
-
-#print X_test
-#print type(X_test),type(y_test)
 
 # ml_predictor.train(training_data, optimize_entire_pipeline=True, optimize_final_model=True)
 ml_predictor.train(training_data)
 
+# ml_predictor.predict_proba(X_test)
+print(ml_predictor.score(X_test, y_test))
+
+
+print "Testing with dataframes"
+training_data=pd.DataFrame.from_dict(training_data)
+testing_data=pd.DataFrame.from_dict(testing_data)
+
+ml_predictor = Predictor(type_of_estimator='classifier', column_descriptions={'sentence':'text','target': 'output'})
+
+X_test, y_test = utils.split_output_dataframe(testing_data, output_column_name='target')
+
+# ml_predictor.train(training_data, optimize_entire_pipeline=True, optimize_final_model=True)
+ml_predictor.train(training_data)
 
 # ml_predictor.predict_proba(X_test)
 print(ml_predictor.score(X_test, y_test))
+
