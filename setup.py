b6b4263c891b252ea796b828c2602630623e8508
ClimbsRocks <climbsbytes@gmail.com>
2016-10-10 19:45:46 -0700

    adds new dependencies, and bumps version to 1.3 for pandas refactor and parallelization

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.2.1',
+    version='1.3',
 
     description='Automated machine learning for production and analytics',
     long_description=long_description,
@@ -83,7 +83,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['scikit-learn', 'xgboost', 'scipy'],
+    install_requires=['scikit-learn', 'xgboost', 'scipy', 'pandas', 'pathos', 'python-dateutil'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
