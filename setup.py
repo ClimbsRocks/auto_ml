2af15ca5290479a98cdba05df5b00bf192ee50a9
ClimbsRocks <climbsbytes@gmail.com>
2016-10-16 22:43:09 -0700

    adds notes on steps to take to publish to pypi

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ See:
 https://packaging.python.org/en/latest/distributing.html
 https://github.com/pypa/sampleproject
 
+# python setup.py sdist bdist_wheel
+# python setup.py register
+# twine upload dist/auto_ml-VERSION_NUMBER*
+
 Built using this blog post as well:
 https://hynek.me/articles/sharing-your-labor-of-love-pypi-quick-and-dirty/
 https://tom-christie.github.io/articles/pypi/
