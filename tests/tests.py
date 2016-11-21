0b5fadfebc49fef23513044cfa38361314b78ffa
ClimbsRocks <ClimbsBytes@gmail.com>
2016-11-21 14:24:54 -0800

    adds tests for saving pipeline and still getting the same results

--- tests/tests.py
+++ tests/tests.py
@@ -7,6 +7,7 @@ sys.path = [os.path.abspath(os.path.dirname(__file__))] + sys.path
 
 from nose.tools import assert_equal, assert_not_equal, with_setup
 
+import dill
 import pandas as pd
 from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
@@ -29,36 +30,39 @@ def get_titanic_binary_classification_dataset():
         print(e)
         dataset_url = 'http://biostat.mc.vanderbilt.edu/wiki/pub/Main/DataSets/titanic3.csv'
         df_titanic = pd.read_csv(dataset_url)
-        df_titanic.to_csv(os.path.join('tests', 'titanic.csv'))
+        # Do not write the index that pandas automatically creates
+        df_titanic.to_csv(os.path.join('tests', 'titanic.csv'), index=False)
     # print(df_titanic)
     df_titanic = df_titanic.drop(['boat', 'body'], axis=1)
     df_titanic_train, df_titanic_test = train_test_split(df_titanic, test_size=0.33, random_state=42)
     return df_titanic_train, df_titanic_test
 
 
-df_titanic_train, df_titanic_test = get_titanic_binary_classification_dataset()
 
 
 def test_binary_classification():
-    df_titanic_train_local = df_titanic_train.drop(['name', 'ticket', 'cabin', 'home.dest'], axis=1)
-    df_titanic_test_local = df_titanic_test.drop(['name', 'ticket', 'cabin', 'home.dest'], axis=1)
+    df_titanic_train, df_titanic_test = get_titanic_binary_classification_dataset()
+    df_titanic_train = df_titanic_train.drop(['name', 'ticket', 'cabin', 'home.dest'], axis=1)
+    df_titanic_test = df_titanic_test.drop(['name', 'ticket', 'cabin', 'home.dest'], axis=1)
     column_descriptions = {
         'survived': 'output'
         , 'embarked': 'categorical'
+        , 'pclass': 'categorical'
     }
 
     ml_predictor = Predictor(type_of_estimator='classifier', column_descriptions=column_descriptions)
-    ml_predictor.train(df_titanic_train_local)
+    ml_predictor.train(df_titanic_train)
 
-    test_score = ml_predictor.score(df_titanic_test_local, df_titanic_test_local.survived)
-    # Right now we're getting a score of -.2136
-    assert test_score > -0.22
+    test_score = ml_predictor.score(df_titanic_test, df_titanic_test.survived, verbose=0)
+    # Right now we're getting a score of -.205
+    # Make sure our score is good, but not unreasonably good
+    assert -0.215 < test_score < -0.17
 
 
 
-df_boston_train, df_boston_test = get_boston_regression_dataset()
 
 def test_regression():
+    df_boston_train, df_boston_test = get_boston_regression_dataset()
     column_descriptions = {
         'MEDV': 'output'
         , 'CHAS': 'categorical'
@@ -67,10 +71,55 @@ def test_regression():
     ml_predictor = Predictor(type_of_estimator='regressor', column_descriptions=column_descriptions)
 
     ml_predictor.train(df_boston_train, verbose=False)
-    test_score = ml_predictor.score(df_boston_test, df_boston_test.MEDV, verbose=False)
-    print(test_score)
+    test_score = ml_predictor.score(df_boston_test, df_boston_test.MEDV, verbose=0)
 
-    # Currently, we expect to get a score of -3.87
-    assert -4 < test_score
+    # Currently, we expect to get a score of -3.09
+    # Make sure our score is good, but not unreasonably good
+    assert -3.2 < test_score < -2.8
+
+
+def test_saving_trained_pipeline_regression():
+    df_boston_train, df_boston_test = get_boston_regression_dataset()
+    column_descriptions = {
+        'MEDV': 'output'
+        , 'CHAS': 'categorical'
+    }
+
+    ml_predictor = Predictor(type_of_estimator='regressor', column_descriptions=column_descriptions)
+
+    ml_predictor.train(df_boston_train, verbose=False)
+
+    file_name = ml_predictor.save()
+
+    with open(file_name, 'rb') as read_file:
+        saved_ml_pipeline = dill.load(read_file)
+
+    test_score = saved_ml_pipeline.score(df_boston_test, df_boston_test.MEDV)
+    # Make sure our score is good, but not unreasonably good
+    assert -3.2 < test_score < -2.8
+
+
+def test_saving_trained_pipeline_binary_classification():
+    df_titanic_train, df_titanic_test = get_titanic_binary_classification_dataset()
+    df_titanic_train = df_titanic_train.drop(['name', 'ticket', 'cabin', 'home.dest'], axis=1)
+    df_titanic_test = df_titanic_test.drop(['name', 'ticket', 'cabin', 'home.dest'], axis=1)
+    column_descriptions = {
+        'survived': 'output'
+        , 'embarked': 'categorical'
+        , 'pclass': 'categorical'
+    }
+
+    ml_predictor = Predictor(type_of_estimator='classifier', column_descriptions=column_descriptions)
+    ml_predictor.train(df_titanic_train)
+
+    file_name = ml_predictor.save()
+
+    with open(file_name, 'rb') as read_file:
+        saved_ml_pipeline = dill.load(read_file)
+
+    test_score = saved_ml_pipeline.score(df_titanic_test, df_titanic_test.survived)
+    # Right now we're getting a score of -.205
+    # Make sure our score is good, but not unreasonably good
+    assert -0.215 < test_score < -0.17
 
 
