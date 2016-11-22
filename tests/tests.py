f5b068576147514b6e8d08209c260f4b6a6ec236
ClimbsRocks <ClimbsBytes@gmail.com>
2016-11-21 16:09:26 -0800

    adds tests for getting predictions one at a time

--- tests/tests.py
+++ tests/tests.py
@@ -2,7 +2,9 @@
 To get standard out, run nosetests as follows:
   $ nosetests -sv tests
 """
-import sys, os
+import datetime
+import os
+import sys
 sys.path = [os.path.abspath(os.path.dirname(__file__))] + sys.path
 
 from nose.tools import assert_equal, assert_not_equal, with_setup
@@ -10,7 +12,6 @@ from nose.tools import assert_equal, assert_not_equal, with_setup
 import dill
 
 import utils_testing as utils
-
 
 def test_binary_classification():
     df_titanic_train, df_titanic_test = utils.get_titanic_binary_classification_dataset()
@@ -57,8 +58,126 @@ def test_saving_trained_pipeline_binary_classification():
 
     test_score = saved_ml_pipeline.score(df_titanic_test, df_titanic_test.survived)
     # Right now we're getting a score of -.205
-    # Make sure our score is good, but not unreasonably good
     assert -0.215 < test_score < -0.17
 
+def test_getting_single_predictions_regression():
+    df_boston_train, df_boston_test = utils.get_boston_regression_dataset()
+    ml_predictor = utils.train_basic_regressor(df_boston_train)
+    file_name = ml_predictor.save()
 
-# def test_getting_single_predictions_regression():
+    with open(file_name, 'rb') as read_file:
+        saved_ml_pipeline = dill.load(read_file)
+
+    df_boston_test_dictionaries = df_boston_test.to_dict('records')
+
+    # 1. make sure the accuracy is the same
+
+    predictions = []
+    for row in df_boston_test_dictionaries:
+        predictions.append(saved_ml_pipeline.predict(row))
+
+    first_score = utils.calculate_rmse(df_boston_test.MEDV, predictions)
+    print('first_score')
+    print(first_score)
+    # Make sure our score is good, but not unreasonably good
+    assert -3.2 < first_score < -2.8
+
+    # 2. make sure the speed is reasonable (do it a few extra times)
+    data_length = len(df_boston_test_dictionaries)
+    start_time = datetime.datetime.now()
+    for idx in range(1000):
+        row_num = idx % data_length
+        saved_ml_pipeline.predict(df_boston_test_dictionaries[row_num])
+    end_time = datetime.datetime.now()
+    duration = end_time - start_time
+
+    print('duration.total_seconds()')
+    print(duration.total_seconds())
+
+    # It's very difficult to set a benchmark for speed that will work across all machines.
+    # On my 2013 bottom of the line 15" MacBook Pro, this runs in about 0.8 seconds for 1000 predictions
+    # That's about 1 millisecond per prediction
+    # Assuming we might be running on a test box that's pretty weak, multiply by 3
+    # Also make sure we're not running unreasonably quickly
+    assert 0.4 < duration.total_seconds() / 1.0 < 3
+
+
+    # 3. make sure we're not modifying the dictionaries (the score is the same after running a few experiments as it is the first time)
+
+    predictions = []
+    for row in df_boston_test_dictionaries:
+        predictions.append(saved_ml_pipeline.predict(row))
+
+    second_score = utils.calculate_rmse(df_boston_test.MEDV, predictions)
+    print('second_score')
+    print(second_score)
+    # Make sure our score is good, but not unreasonably good
+    assert -3.2 < second_score < -2.8
+
+
+
+
+def test_getting_single_predictions_classification():
+    df_titanic_train, df_titanic_test = utils.get_titanic_binary_classification_dataset()
+    ml_predictor = utils.train_basic_binary_classifier(df_titanic_train)
+    file_name = ml_predictor.save()
+
+    with open(file_name, 'rb') as read_file:
+        saved_ml_pipeline = dill.load(read_file)
+
+    df_titanic_test_dictionaries = df_titanic_test.to_dict('records')
+
+    # 1. make sure the accuracy is the same
+
+    predictions = []
+    for row in df_titanic_test_dictionaries:
+        predictions.append(saved_ml_pipeline.predict_proba(row)[1])
+
+    print('predictions')
+    print(predictions)
+
+    first_score = utils.calculate_brier_score_loss(df_titanic_test.survived, predictions)
+    print('first_score')
+    print(first_score)
+    # Make sure our score is good, but not unreasonably good
+    assert -0.215 < first_score < -0.17
+
+    # 2. make sure the speed is reasonable (do it a few extra times)
+    data_length = len(df_titanic_test_dictionaries)
+    start_time = datetime.datetime.now()
+    for idx in range(1000):
+        row_num = idx % data_length
+        saved_ml_pipeline.predict(df_titanic_test_dictionaries[row_num])
+    end_time = datetime.datetime.now()
+    duration = end_time - start_time
+
+    print('duration.total_seconds()')
+    print(duration.total_seconds())
+
+    # It's very difficult to set a benchmark for speed that will work across all machines.
+    # On my 2013 bottom of the line 15" MacBook Pro, this runs in about 0.8 seconds for 1000 predictions
+    # That's about 1 millisecond per prediction
+    # Assuming we might be running on a test box that's pretty weak, multiply by 3
+    # Also make sure we're not running unreasonably quickly
+    assert 0.4 < duration.total_seconds() < 3
+
+
+    # 3. make sure we're not modifying the dictionaries (the score is the same after running a few experiments as it is the first time)
+
+    predictions = []
+    for row in df_titanic_test_dictionaries:
+        predictions.append(saved_ml_pipeline.predict_proba(row)[1])
+
+    print('predictions')
+    print(predictions)
+    print('df_titanic_test_dictionaries')
+    print(df_titanic_test_dictionaries)
+    second_score = utils.calculate_brier_score_loss(df_titanic_test.survived, predictions)
+    print('second_score')
+    print(second_score)
+    # Make sure our score is good, but not unreasonably good
+    assert -0.215 < second_score < -0.17
+
+
+
+
