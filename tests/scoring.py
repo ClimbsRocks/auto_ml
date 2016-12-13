2052132cf80bf4f02d0048f89da6830592ffc877
ClimbsRocks <ClimbsBytes@gmail.com>
2016-12-12 19:32:28 -0800

    adds test for letting the user pass in their own custom scoring metric

---
+++ tests/scoring.py
@@ -0,0 +1,34 @@
+import os
+import sys
+sys.path = [os.path.abspath(os.path.dirname(__file__))] + sys.path
+
+from auto_ml import Predictor
+
+import utils_testing as utils
+
+def always_return_ten_thousand(estimator=None, actuals=None, probas=None):
+    return 10000
+
+def test_binary_classification():
+    df_titanic_train, df_titanic_test = utils.get_titanic_binary_classification_dataset()
+
+    column_descriptions = {
+        'survived': 'output'
+        , 'embarked': 'categorical'
+        , 'pclass': 'categorical'
+    }
+
+    ml_predictor = Predictor(type_of_estimator='classifier', column_descriptions=column_descriptions)
+
+    ml_predictor.train(df_titanic_train, scoring=always_return_ten_thousand)
+
+    test_score = ml_predictor.score(df_titanic_test, df_titanic_test.survived)
+
+    print('test_score')
+    print(test_score)
+
+    assert test_score == -10000
+
+
+
+
