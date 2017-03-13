aa09996d4ac3408ba165fc32c103d514f11c859a
ClimbsRocks <climbsbytes@gmail.com>
2017-03-12 21:41:29 -0700

    simplifies down travis tests to see what errors we get when trying to import keras

---
+++ keras_import.py
@@ -0,0 +1,4 @@
+from keras.constraints import maxnorm
+from keras.layers import Dense, Dropout
+from keras.models import Sequential
+from keras.wrappers.scikit_learn import KerasRegressor, KerasClassifier
