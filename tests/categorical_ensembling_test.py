52962d45ff52fe333af99b9f5a81d55c130b9eff
ClimbsRocks <ClimbsBytes@gmail.com>
2017-04-04 14:59:34 -0700

    adds basic test for categorical ensembling

---
+++ tests/categorical_ensembling_test.py
@@ -0,0 +1,37 @@
+import os
+import sys
+sys.path = [os.path.abspath(os.path.dirname(__file__))] + sys.path
+
+from auto_ml import Predictor
+
+import dill
+import numpy as np
+from nose.tools import assert_equal, assert_not_equal, with_setup
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+import utils_testing as utils
+
+def test_categorical_ensemble_basic_classifier():
+    np.random.seed(0)
+
+    df_titanic_train, df_titanic_test = utils.get_titanic_binary_classification_dataset()
+
+    column_descriptions = {
+        'survived': 'output'
+        , 'embarked': 'categorical'
+    }
+
+    ml_predictor = Predictor(type_of_estimator='classifier', column_descriptions=column_descriptions)
+
+    ml_predictor.train_categorical_ensemble(df_titanic_train, categorical_column='pclass', optimize_final_model=True)
+
+    test_score = ml_predictor.score(df_titanic_test, df_titanic_test.survived)
+
+    print('test_score')
+    print(test_score)
+
+    # Small sample sizes mean there's a fair bit of noise here
+    assert -0.226 < test_score < -0.17
+
+
