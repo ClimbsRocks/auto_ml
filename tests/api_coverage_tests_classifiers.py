a0a7e8277e36cb8303665a895f3769aefa4199aa
ClimbsRocks <ClimbsBytes@gmail.com>
2017-04-19 13:46:41 -0700

    code clean up
    adjusts expectations upwards for nlp

--- tests/api_coverage_tests_classifiers.py
+++ tests/api_coverage_tests_classifiers.py
@@ -17,29 +17,6 @@ from sklearn.model_selection import train_test_split
 
 import utils_testing as utils
 
-# def test_optimize_final_model_classification(model_name=None):
-#     np.random.seed(0)
-
-#     df_titanic_train, df_titanic_test = utils.get_titanic_binary_classification_dataset()
-
-#     column_descriptions = {
-#         'survived': 'output'
-#         , 'embarked': 'categorical'
-#         , 'pclass': 'categorical'
-#     }
-
-#     ml_predictor = Predictor(type_of_estimator='classifier', column_descriptions=column_descriptions)
-
-#     ml_predictor.train(df_titanic_train, optimize_final_model=True, model_names=model_name)
-
-#     test_score = ml_predictor.score(df_titanic_test, df_titanic_test.survived)
-
-#     print('test_score')
-#     print(test_score)
-
-#     # Small sample sizes mean there's a fair bit of noise here
-#     assert -0.226 < test_score < -0.17
-
 def test_perform_feature_selection_true_classification(model_name=None):
     np.random.seed(0)
 
@@ -239,29 +216,6 @@ def test_binary_classification_predict_proba_on_Predictor_instance(model_name=No
     assert -0.215 < test_score < -0.17
 
 
-def test_nlp_multilabel_classification(model_name=None):
-    np.random.seed(0)
-
-    df_twitter_train, df_twitter_test = utils.get_twitter_sentiment_multilabel_classification_dataset()
-
-    column_descriptions = {
-        'airline_sentiment': 'output'
-        , 'airline': 'categorical'
-        , 'text': 'nlp'
-        , 'tweet_location': 'categorical'
-        , 'user_timezone': 'categorical'
-        , 'tweet_created': 'date'
-    }
-
-    ml_predictor = Predictor(type_of_estimator='classifier', column_descriptions=column_descriptions)
-    ml_predictor.train(df_twitter_train, model_names=model_name)
-
-    test_score = ml_predictor.score(df_twitter_test, df_twitter_test.airline_sentiment)
-
-    # Make sure our score is good, but not unreasonably good
-    print('test_score')
-    print(test_score)
-    assert 0.67 < test_score < 0.79
 
 # For some reason, this test now causes a Segmentation Default on travis when run on python 3.5.
 # home/travis/.travis/job_stages: line 53:  8810 Segmentation fault      (core dumped) nosetests -v --with-coverage --cover-package auto_ml tests
@@ -472,9 +426,9 @@ if os.environ.get('TRAVIS_PYTHON_VERSION', '0') != '3.5':
         print('first_score')
         print(first_score)
         # Make sure our score is good, but not unreasonably good
-        lower_bound = 0.67
-        if model_name == 'LGBMClassifier':
-            lower_bound = 0.655
+        lower_bound = 0.73
+        # if model_name == 'LGBMClassifier':
+        #     lower_bound = 0.655
         assert lower_bound < first_score < 0.79
 
         # 2. make sure the speed is reasonable (do it a few extra times)
@@ -517,186 +471,26 @@ if os.environ.get('TRAVIS_PYTHON_VERSION', '0') != '3.5':
         assert lower_bound < second_score < 0.79
 
 
-
-
-# def test_optimize_entire_pipeline_classification():
-#     np.random.seed(0)
-
-#     df_titanic_train, df_titanic_test = utils.get_titanic_binary_classification_dataset()
-
-#     column_descriptions = {
-#         'survived': 'output'
-#         , 'embarked': 'categorical'
-#         , 'pclass': 'categorical'
-#     }
-
-#     ml_predictor = Predictor(type_of_estimator='classifier', column_descriptions=column_descriptions)
-
-#     ml_predictor.train(df_titanic_train, optimize_entire_pipeline=True, model_names=model_name)
-
-#     test_score = ml_predictor.score(df_titanic_test, df_titanic_test.survived)
-
-#     print('test_score')
-#     print(test_score)
-
-#     assert -0.215 < test_score < -0.17
-
-
-# def test_X_test_and_y_test_classification():
-#     np.random.seed(0)
-
-#     df_titanic_train, df_titanic_test = utils.get_titanic_binary_classification_dataset()
-
-#     column_descriptions = {
-#         'survived': 'output'
-#         , 'embarked': 'categorical'
-#         , 'pclass': 'categorical'
-#     }
-
-#     ml_predictor = Predictor(type_of_estimator='classifier', column_descriptions=column_descriptions)
-
-#     ml_predictor.train(df_titanic_train, X_test=df_titanic_test, y_test=df_titanic_test.survived, model_names=model_name)
-
-#     test_score = ml_predictor.score(df_titanic_test, df_titanic_test.survived)
-
-#     print('test_score')
-#     print(test_score)
-
-#     assert -0.215 < test_score < -0.17
-
-
-# def test_compute_power_1_classification():
-#     np.random.seed(0)
-
-#     df_titanic_train, df_titanic_test = utils.get_titanic_binary_classification_dataset()
-
-#     column_descriptions = {
-#         'survived': 'output'
-#         , 'embarked': 'categorical'
-#         , 'pclass': 'categorical'
-#     }
-
-#     ml_predictor = Predictor(type_of_estimator='classifier', column_descriptions=column_descriptions)
-
-#     ml_predictor.train(df_titanic_train, compute_power=1, model_names=model_name)
-
-#     test_score = ml_predictor.score(df_titanic_test, df_titanic_test.survived)
-
-#     print('test_score')
-#     print(test_score)
-
-#     assert -0.215 < test_score < -0.17
-
-
-# This test passes, but takes a long time to run. deprecating it for now until we rethink what we really want compute_power to accomplish
-# def test_compute_power_9_classification():
-#     np.random.seed(0)
-
-#     df_titanic_train, df_titanic_test = utils.get_titanic_binary_classification_dataset()
-
-#     column_descriptions = {
-#         'survived': 'output'
-#         , 'embarked': 'categorical'
-#         , 'pclass': 'categorical'
-#     }
-
-#     ml_predictor = Predictor(type_of_estimator='classifier', column_descriptions=column_descriptions)
-
-#     ml_predictor.train(df_titanic_train, compute_power=9, model_names=model_name)
-
-#     test_score = ml_predictor.score(df_titanic_test, df_titanic_test.survived)
-
-#     print('test_score')
-#     print(test_score)
-
-#     assert -0.215 < test_score < -0.17
-
-
-# If the user passes in X_test and y_test, we will use those to determine the best model, rather than CV scores
-# def test_select_from_multiple_classification_models_using_X_test_and_y_test():
-#     np.random.seed(0)
-
-#     df_titanic_train, df_titanic_test = utils.get_titanic_binary_classification_dataset()
-
-#     column_descriptions = {
-#         'survived': 'output'
-#         , 'embarked': 'categorical'
-#         , 'pclass': 'categorical'
-#     }
-
-#     ml_predictor = Predictor(type_of_estimator='classifier', column_descriptions=column_descriptions)
-
-#     ml_predictor.train(df_titanic_train, model_names=['LogisticRegression', 'RandomForestClassifier', 'RidgeClassifier', 'GradientBoostingClassifier', 'ExtraTreesClassifier', 'AdaBoostClassifier', 'SGDClassifier', 'Perceptron', 'PassiveAggressiveClassifier'], X_test=df_titanic_test, y_test=df_titanic_test.survived, model_names=model_name)
-
-#     test_score = ml_predictor.score(df_titanic_test, df_titanic_test.survived)
-
-#     print('test_score')
-#     print(test_score)
-
-#     assert -0.215 < test_score < -0.17
-
-
-
-# I'm not sure what a reasonable value here would be
-# def test_multilabel_classification_predict_proba_on_Predictor_instance():
-#     np.random.seed(0)
-
-#     df_twitter_train, df_twitter_test = utils.get_twitter_sentiment_multilabel_classification_dataset()
-#     ml_predictor = utils.train_basic_multilabel_classifier(df_twitter_train)
-
-#     predictions = ml_predictor.predict_proba(df_twitter_test)
-#     test_score = accuracy_score(predictions, df_twitter_test.airline_sentiment)
-#     # Right now we're getting a score of -.205
-#     # Make sure our score is good, but not unreasonably good
-#     print('test_score')
-#     print(test_score)
-#     assert 0.67 < test_score < 0.79
-
-
-
-
-# Right now this just takes forever to run. Will have to look into optimizing
-# def test_compute_power_10():
-    # np.random.seed(0)
-
-#     df_titanic_train, df_titanic_test = utils.get_titanic_binary_classification_dataset()
-
-#     # This test tries something like 2,000 different combinations of hyperparameters for the pipeline
-#     # To make this more reasonable, we'll cut down the data size to be a fraction of it's full size, so we are just testing whether everything runs
-#     df_titanic_train, df_titanic_train_ignore = train_test_split(df_titanic_train, train_size=0.1, random_state=42)
-
-
-#     column_descriptions = {
-#         'survived': 'output'
-#         , 'embarked': 'categorical'
-#         , 'pclass': 'categorical'
-#     }
-
-#     ml_predictor = Predictor(type_of_estimator='classifier', column_descriptions=column_descriptions)
-
-#     ml_predictor.train(df_titanic_train, compute_power=10, model_names=model_name)
-
-#     test_score = ml_predictor.score(df_titanic_test, df_titanic_test.survived)
-
-#     print('test_score')
-#     print(test_score)
-
-#     assert -0.215 < test_score < -0.17
-
-
-# TODO: run tests for each of the different models
-# TODO: test for picking the best model automatically
-# optimize_entire_pipeline
-# X_test, y_test
-# take_log_of_y
-# ideally something about XGB not needing to be installed, but running if it is installed
-# Dates
-# NLP
-# train ml ensemble
-# ideally use XGB
-# print results for linear model
-# Try passing in a list of dictionaries everywhere (train, predict, predict_proba, score, on both the saved pipeline and the ml_predictor object)
-# call predict and predict_proba on the ml_predictor object, not just the final pipeline
-# Test the num_features for standard problem (should be all of them). Then test num_features if we pass in a ton of duplicate columns where it should be removing some features. then same thing, but perform_feature_selection=False. we can get the num_features from the final trained model. even if some features are not useful, we should be able to see the number of features the model has considered.
-
-
+def test_nlp_multilabel_classification(model_name=None):
+    np.random.seed(0)
+
+    df_twitter_train, df_twitter_test = utils.get_twitter_sentiment_multilabel_classification_dataset()
+
+    column_descriptions = {
+        'airline_sentiment': 'output'
+        , 'airline': 'categorical'
+        , 'text': 'nlp'
+        , 'tweet_location': 'categorical'
+        , 'user_timezone': 'categorical'
+        , 'tweet_created': 'date'
+    }
+
+    ml_predictor = Predictor(type_of_estimator='classifier', column_descriptions=column_descriptions)
+    ml_predictor.train(df_twitter_train, model_names=model_name)
+
+    test_score = ml_predictor.score(df_twitter_test, df_twitter_test.airline_sentiment)
+
+    # Make sure our score is good, but not unreasonably good
+    print('test_score')
+    print(test_score)
+    assert 0.73 < test_score < 0.79
