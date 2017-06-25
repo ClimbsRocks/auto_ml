2ca215e06ad933a9cec94bbb96adff69f3583ebc
ClimbsRocks <climbsbytes@gmail.com>
2017-06-25 10:46:57 -0700

    reorders some tests

--- tests/basic_tests.py
+++ tests/basic_tests.py
@@ -18,103 +18,6 @@ import dill
 import numpy as np
 import utils_testing as utils
 
-
-def test_linear_model_analytics_classification(model_name=None):
-    np.random.seed(0)
-
-    df_titanic_train, df_titanic_test = utils.get_titanic_binary_classification_dataset()
-
-    column_descriptions = {
-        'survived': 'output'
-        , 'embarked': 'categorical'
-        , 'pclass': 'categorical'
-    }
-
-    ml_predictor = Predictor(type_of_estimator='classifier', column_descriptions=column_descriptions)
-
-    ml_predictor.train(df_titanic_train, model_names='RidgeClassifier')
-
-    test_score = ml_predictor.score(df_titanic_test, df_titanic_test.survived)
-
-    print('test_score')
-    print(test_score)
-
-    # Linear models aren't super great on this dataset...
-    assert -0.37 < test_score < -0.17
-
-
-def test_all_algos_classification(model_name=None):
-    np.random.seed(0)
-
-    df_titanic_train, df_titanic_test = utils.get_titanic_binary_classification_dataset()
-
-    column_descriptions = {
-        'survived': 'output'
-        , 'embarked': 'categorical'
-        , 'pclass': 'categorical'
-    }
-
-    ml_predictor = Predictor(type_of_estimator='classifier', column_descriptions=column_descriptions)
-
-    ml_predictor.train(df_titanic_train, model_names=['LogisticRegression', 'RandomForestClassifier', 'RidgeClassifier', 'GradientBoostingClassifier', 'ExtraTreesClassifier', 'AdaBoostClassifier', 'SGDClassifier', 'Perceptron', 'PassiveAggressiveClassifier', 'DeepLearningClassifier', 'XGBClassifier', 'LGBMClassifier', 'LinearSVC'])
-
-    test_score = ml_predictor.score(df_titanic_test, df_titanic_test.survived)
-
-    print('test_score')
-    print(test_score)
-
-    assert -0.215 < test_score < -0.17
-
-def test_all_algos_regression():
-    # a random seed of 42 has ExtraTreesRegressor getting the best CV score, and that model doesn't generalize as well as GradientBoostingRegressor.
-    np.random.seed(0)
-
-    df_boston_train, df_boston_test = utils.get_boston_regression_dataset()
-
-    column_descriptions = {
-        'MEDV': 'output'
-        , 'CHAS': 'categorical'
-    }
-
-    ml_predictor = Predictor(type_of_estimator='regressor', column_descriptions=column_descriptions)
-
-    ml_predictor.train(df_boston_train, model_names=['LinearRegression', 'RandomForestRegressor', 'Ridge', 'GradientBoostingRegressor', 'ExtraTreesRegressor', 'AdaBoostRegressor', 'SGDRegressor', 'PassiveAggressiveRegressor', 'Lasso', 'LassoLars', 'ElasticNet', 'OrthogonalMatchingPursuit', 'BayesianRidge', 'ARDRegression', 'MiniBatchKMeans', 'DeepLearningRegressor', 'XGBRegressor', 'XGBClassifier',  'LinearSVR'])
-
-    test_score = ml_predictor.score(df_boston_test, df_boston_test.MEDV)
-
-    print('test_score')
-    print(test_score)
-
-    assert -3.35 < test_score < -2.8
-
-def test_input_df_unmodified():
-    np.random.seed(42)
-
-    df_boston_train, df_boston_test = utils.get_boston_regression_dataset()
-
-    column_descriptions = {
-        'MEDV': 'output'
-        , 'CHAS': 'categorical'
-    }
-
-    ml_predictor = Predictor(type_of_estimator='regressor', column_descriptions=column_descriptions)
-
-    df_shape = df_boston_train.shape
-    ml_predictor.train(df_boston_train)
-
-    training_shape = df_boston_train.shape
-    assert training_shape[0] == df_shape[0]
-    assert training_shape[1] == df_shape[1]
-
-
-    test_score = ml_predictor.score(df_boston_test, df_boston_test.MEDV)
-
-    print('test_score')
-    print(test_score)
-
-    assert -3.35 < test_score < -2.8
-
-
 def test_is_backwards_compatible_with_models_trained_using_1_9_6():
     np.random.seed(0)
 
@@ -176,4 +79,101 @@ def test_is_backwards_compatible_with_models_trained_using_1_9_6():
     # Make sure our score is good, but not unreasonably good
 
     assert -2.8 < second_score < -2.1
+
+
+def test_all_algos_classification(model_name=None):
+    np.random.seed(0)
+
+    df_titanic_train, df_titanic_test = utils.get_titanic_binary_classification_dataset()
+
+    column_descriptions = {
+        'survived': 'output'
+        , 'embarked': 'categorical'
+        , 'pclass': 'categorical'
+    }
+
+    ml_predictor = Predictor(type_of_estimator='classifier', column_descriptions=column_descriptions)
+
+    ml_predictor.train(df_titanic_train, model_names=['LogisticRegression', 'RandomForestClassifier', 'RidgeClassifier', 'GradientBoostingClassifier', 'ExtraTreesClassifier', 'AdaBoostClassifier', 'SGDClassifier', 'Perceptron', 'PassiveAggressiveClassifier', 'DeepLearningClassifier', 'XGBClassifier', 'LGBMClassifier', 'LinearSVC'])
+
+    test_score = ml_predictor.score(df_titanic_test, df_titanic_test.survived)
+
+    print('test_score')
+    print(test_score)
+
+    assert -0.215 < test_score < -0.17
+
+def test_linear_model_analytics_classification(model_name=None):
+    np.random.seed(0)
+
+    df_titanic_train, df_titanic_test = utils.get_titanic_binary_classification_dataset()
+
+    column_descriptions = {
+        'survived': 'output'
+        , 'embarked': 'categorical'
+        , 'pclass': 'categorical'
+    }
+
+    ml_predictor = Predictor(type_of_estimator='classifier', column_descriptions=column_descriptions)
+
+    ml_predictor.train(df_titanic_train, model_names='RidgeClassifier')
+
+    test_score = ml_predictor.score(df_titanic_test, df_titanic_test.survived)
+
+    print('test_score')
+    print(test_score)
+
+    # Linear models aren't super great on this dataset...
+    assert -0.37 < test_score < -0.17
+
+
+def test_all_algos_regression():
+    # a random seed of 42 has ExtraTreesRegressor getting the best CV score, and that model doesn't generalize as well as GradientBoostingRegressor.
+    np.random.seed(0)
+
+    df_boston_train, df_boston_test = utils.get_boston_regression_dataset()
+
+    column_descriptions = {
+        'MEDV': 'output'
+        , 'CHAS': 'categorical'
+    }
+
+    ml_predictor = Predictor(type_of_estimator='regressor', column_descriptions=column_descriptions)
+
+    ml_predictor.train(df_boston_train, model_names=['LinearRegression', 'RandomForestRegressor', 'Ridge', 'GradientBoostingRegressor', 'ExtraTreesRegressor', 'AdaBoostRegressor', 'SGDRegressor', 'PassiveAggressiveRegressor', 'Lasso', 'LassoLars', 'ElasticNet', 'OrthogonalMatchingPursuit', 'BayesianRidge', 'ARDRegression', 'MiniBatchKMeans', 'DeepLearningRegressor', 'XGBRegressor', 'XGBClassifier',  'LinearSVR'])
+
+    test_score = ml_predictor.score(df_boston_test, df_boston_test.MEDV)
+
+    print('test_score')
+    print(test_score)
+
+    assert -3.35 < test_score < -2.8
+
+def test_input_df_unmodified():
+    np.random.seed(42)
+
+    df_boston_train, df_boston_test = utils.get_boston_regression_dataset()
+
+    column_descriptions = {
+        'MEDV': 'output'
+        , 'CHAS': 'categorical'
+    }
+
+    ml_predictor = Predictor(type_of_estimator='regressor', column_descriptions=column_descriptions)
+
+    df_shape = df_boston_train.shape
+    ml_predictor.train(df_boston_train)
+
+    training_shape = df_boston_train.shape
+    assert training_shape[0] == df_shape[0]
+    assert training_shape[1] == df_shape[1]
+
+
+    test_score = ml_predictor.score(df_boston_test, df_boston_test.MEDV)
+
+    print('test_score')
+    print(test_score)
+
+    assert -3.35 < test_score < -2.8
+
 
