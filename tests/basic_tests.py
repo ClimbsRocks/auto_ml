3799632693543c838bf48369d236fa3a1e20c5a1
ClimbsRocks <ClimbsBytes@gmail.com>
2017-09-11 18:13:21 -0700

    removes old test

--- tests/basic_tests.py
+++ tests/basic_tests.py
@@ -20,68 +20,6 @@ import dill
 import numpy as np
 import utils_testing as utils
 
-def test_is_backwards_compatible_with_models_trained_using_1_9_6():
-    np.random.seed(0)
-
-    df_boston_train, df_boston_test = utils.get_boston_regression_dataset()
-
-    with open(os.path.join('tests','trained_ml_model_v_1_9_6.dill'), 'rb') as read_file:
-        saved_ml_pipeline = dill.load(read_file)
-
-    df_boston_test_dictionaries = df_boston_test.to_dict('records')
-
-    # 1. make sure the accuracy is the same
-
-    predictions = []
-    for row in df_boston_test_dictionaries:
-        predictions.append(saved_ml_pipeline.predict(row))
-
-    print('predictions')
-    print(predictions)
-    print('predictions[0]')
-    print(predictions[0])
-    print('type(predictions)')
-    print(type(predictions))
-    first_score = utils.calculate_rmse(df_boston_test.MEDV, predictions)
-    print('first_score')
-    print(first_score)
-    # Make sure our score is good, but not unreasonably good
-
-    assert -2.8 < first_score < -2.1
-
-    # 2. make sure the speed is reasonable (do it a few extra times)
-    data_length = len(df_boston_test_dictionaries)
-    start_time = datetime.datetime.now()
-    for idx in range(1000):
-        row_num = idx % data_length
-        saved_ml_pipeline.predict(df_boston_test_dictionaries[row_num])
-    end_time = datetime.datetime.now()
-    duration = end_time - start_time
-
-    print('duration.total_seconds()')
-    print(duration.total_seconds())
-
-    # It's very difficult to set a benchmark for speed that will work across all machines.
-    # On my 2013 bottom of the line 15" MacBook Pro, this runs in about 0.8 seconds for 1000 predictions
-    # That's about 1 millisecond per prediction
-    # Assuming we might be running on a test box that's pretty weak, multiply by 3
-    # Also make sure we're not running unreasonably quickly
-    assert 0.1 < duration.total_seconds() / 1.0 < 15
-
-
-    # 3. make sure we're not modifying the dictionaries (the score is the same after running a few experiments as it is the first time)
-
-    predictions = []
-    for row in df_boston_test_dictionaries:
-        predictions.append(saved_ml_pipeline.predict(row))
-
-    second_score = utils.calculate_rmse(df_boston_test.MEDV, predictions)
-    print('second_score')
-    print(second_score)
-    # Make sure our score is good, but not unreasonably good
-
-    assert -2.8 < second_score < -2.1
-
 
 def test_all_algos_classification(model_name=None):
     np.random.seed(0)
