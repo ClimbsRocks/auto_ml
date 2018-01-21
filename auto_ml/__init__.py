c0de0415d23c4f3f97e0479d92cdeeeedd4ff6af
ClimbsRocks <climbsbytes@gmail.com>
2018-01-21 13:49:36 -0800

    makes load_ml_model more easily importable

--- auto_ml/__init__.py
+++ auto_ml/__init__.py
@@ -1,7 +1,3 @@
-"""
-For examples, please reference https://github.com/ClimbsRocks/auto_ml
-
-"""
-
 from auto_ml.predictor import Predictor
 from auto_ml._version import __version__
+from auto_ml.utils_models import load_ml_model
